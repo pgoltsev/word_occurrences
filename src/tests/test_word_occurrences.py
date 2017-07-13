47684f4b82a14952d49519323487fc2b21049d2e
Pavel Goltsev <p.goltsev@gmail.com>
2017-07-13 15:50:38 +0300

    Initial commit.

---
+++ src/tests/test_word_occurrences.py
@@ -0,0 +1,104 @@
+import subprocess
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+from unittest import TestCase, mock
+
+import word_occurrences
+
+
+class WordOccurrencesFunctionsTestCase(TestCase):
+    """
+    Test separate functions of the script.
+    """
+
+    def test_split_words_func(self):
+        expected = ['occurrences', 'mary\'s', 'some', 'text', 'texts', 'with']
+
+        actual = word_occurrences.split_to_word_func(
+            'some. mary\'s text\'! ;with:?! , texts, ,occurrences?'
+        )
+
+        self.assertListEqual(sorted(actual), sorted(expected))
+
+    def test_count_word_occurrences(self):
+        expected = {'w1': 1, 'w2': 2, 'w3': 3}
+
+        actual = dict(word_occurrences.count_word_occurrences(
+            'w3 w1 w2 w3 w2 w3',
+            split_func=str.split
+        ))
+
+        self.assertDictEqual(actual, expected)
+
+    def test_sorting(self):
+        expected = (('w1', 3), ('w2', 2), ('w3', 2), ('w4', 1))
+
+        actual = word_occurrences.sort_by_occurrences(
+            {'w4': 1, 'w2': 2, 'w1': 3, 'w3': 2}
+        )
+
+        self.assertListEqual(list(actual), list(expected))
+
+    def test_format_with_colon(self):
+        expected = 'w1: 3'
+
+        actual = word_occurrences.format_with_colon(('w1', 3))
+
+        self.assertEqual(actual, expected)
+
+    def test_print_to_stdout(self):
+        with mock.patch('sys.stdout.write') as mocked_write:
+            word_occurrences.print_statistic(
+                (('w1', 3), ('w2', 2), ('w3', 2), ('w4', 1)),
+                formatter=lambda item: f'{item[0]}: {item[1]}'
+            )
+
+        mocked_write.assert_has_calls([
+            mock.call('w1: 3\n'),
+            mock.call('w2: 2\n'),
+            mock.call('w3: 2\n'),
+            mock.call('w4: 1\n'),
+        ])
+
+
+class WordOccurrencesScriptTestCase(TestCase):
+    """
+    Test the script itself.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        script_path = Path.cwd() / Path('word_occurrences.py')
+        cls.run_args = ('python3', script_path)
+
+    def test_script_needs_filename(self):
+        with self.assertRaises(subprocess.CalledProcessError) as exc:
+            subprocess.check_output(self.run_args, stderr=subprocess.PIPE)
+
+        self.assertIn(
+            'error: the following arguments are required: file',
+            exc.exception.stderr.decode()
+        )
+
+    def test_script_output(self):
+        string = 'Some, :word\'s! ,occurrences, :,data? to check word\'s ' \
+                 'occurrences data'
+        tmp_file = NamedTemporaryFile()
+        tmp_file.write(string.encode())
+        tmp_file.flush()
+
+        expected = 'data: 2\n' \
+                   'occurrences: 2\n' \
+                   'word\'s: 2\n' \
+                   'Some: 1\n' \
+                   'check: 1\n' \
+                   'to: 1\n'
+
+        try:
+            actual = subprocess.check_output(self.run_args + (tmp_file.name,))
+        finally:
+            tmp_file.close()
+
+        self.assertEqual(actual, expected.encode())
---
+++ src/word_occurrences.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+
+import argparse
+import re
+import sys
+from operator import itemgetter
+from typing import Mapping, Iterable, Counter, Callable, Tuple, Any
+
+WordCountMapping = Mapping[str, int]
+
+
+def print_statistic(iterable: Iterable,
+                    formatter: Callable[[Any], str]) -> None:
+    """
+    Print mapping statistics to the standard output.
+
+    :param iterable: Iterable object to print.
+    :param formatter: Formatter to which an every item of iterable object is
+    passed. It should return a formatted string.
+    """
+    for item in iterable:
+        sys.stdout.write('%s\n' % formatter(item))
+
+
+def format_with_colon(item: Tuple) -> str:
+    """
+    Concatenate key and value with colon.
+
+    :param item: A tuple of two elements.
+    :return: String with values of the tuple with a colon between them.
+    """
+    return f'{item[0]}: {item[1]}'
+
+
+def sort_by_occurrences(mapping: WordCountMapping) -> Iterable[
+    Tuple[str, int]
+]:
+    """
+    Sort mapping by keys in alphabetical order and if they are equal by values
+    in descending order.
+
+    :param mapping: Mapping.
+    :return: Iterable object with sorted sequence of tuples. Every tuple has
+    two elements: a key and a value.
+    """
+    items = mapping.items()
+    items_sorted_by_keys = sorted(items, key=itemgetter(0))
+    items_sorted = sorted(items_sorted_by_keys,
+                          key=itemgetter(1),
+                          reverse=True)
+    return items_sorted
+
+
+def count_word_occurrences(
+        text: str,
+        split_func: Callable[[str], Iterable[str]],
+        accumulator: Counter = None
+) -> Counter:
+    """
+    Count word occurrences in a text.
+
+    :param text: Input text
+    :param split_func: Function for splitting the text into words.
+    :param accumulator: Accumulate result here. May be used for reducing of
+    memory usage. If not defined it will be created.
+    :return: Counter object which key is a word and a value is
+    occurrences of this word in the given text.
+    """
+    if accumulator is None:
+        accumulator = Counter()
+
+    for word in split_func(text):
+        if word:
+            accumulator[word] += 1
+    return accumulator
+
+
+word_regex = re.compile(r'\b([\w\']+)\b')
+"""
+Regular expression of a word. It takes into account an apostrophe.
+"""
+
+
+def split_to_word_func(text: str) -> Iterable[str]:
+    """
+    Split text into words.
+
+    :param text: Text to split.
+    :return: Iterable object with words.
+    """
+    for word in word_regex.finditer(text):
+        yield word[0]
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="""Script counts word occurrences inside of the 
+given text file."""
+    )
+    parser.add_argument(
+        'file',
+        help='path to a text file',
+        type=argparse.FileType('r')
+    )
+    args = parser.parse_args()
+
+    word_occurrences = Counter()
+    for line in args.file:
+        count_word_occurrences(line,
+                               split_func=split_to_word_func,
+                               accumulator=word_occurrences)
+
+    sorted_word_occurrences = sort_by_occurrences(word_occurrences)
+    print_statistic(sorted_word_occurrences,
+                    formatter=format_with_colon)
